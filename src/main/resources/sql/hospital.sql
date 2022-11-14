bcb0ac713fcde3598c50357f5178a4a6de10c79e
dmdwns2 <ssreki1@naver.com>
2022-11-14 13:34:20 +0900

    [스프링부트 핵심가이드] like 검색 hospital.sql 추가
    sql 수원시, 피부과 like 검색

--- src/main/resources/sql/hospital.sql
+++ src/main/resources/sql/hospital.sql
@@ -1,3 +1,6 @@
 SELECT * FROM `likelion-db`.nation_wide_hospitals;
-SELECT road_name_address FROM `likelion-db`.nation_wide_hospitals
-                         WHERE road_name_address LIKE '%수원%';
+SELECT road_name_address, hospital_name
+FROM `likelion-db`.nation_wide_hospitals
+WHERE road_name_address
+LIKE '수원시%'
+AND hospital_name LIKE '피부과%';
