1003b2e7eadc26deaae69dcceff9c9c42ae5514d
dmdwns2 <ssreki1@naver.com>
2022-11-16 11:18:51 +0900

    [스프링부트 핵심가이드] ArticleRestControllerTest 작성
    1. ArticleResponse 추가

--- src/test/java/com/example/springmustachehospital/controller/ArticleRestControllerTest.java
+++ src/test/java/com/example/springmustachehospital/controller/ArticleRestControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.springmustachehospital.controller;
 
+import com.example.springmustachehospital.domain.ArticleResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 @WebMvcTest(ArticleRestController.class)
 class ArticleRestControllerTest {
@@ -21,6 +23,12 @@ class ArticleRestControllerTest {
     @Test
     @DisplayName("등록이 잘 되는지")
     public void response(){
+        ArticleResponse articleResponse = ArticleResponse.builder()
+                .id((long)1)
+                .title("하이")
+                .content("바이")
+                .build();
 
+        given()
     }
 }
