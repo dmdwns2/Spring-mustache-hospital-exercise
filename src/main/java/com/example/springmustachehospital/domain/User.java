a7568c0f0856b29e5ef6a0d627c54f8776fc15e3
dmdwns2 <ssreki1@naver.com>
2022-11-17 11:50:14 +0900

    [스프링부트 핵심가이드] User 추가

---
+++ src/main/java/com/example/springmustachehospital/domain/User.java
@@ -0,0 +1,27 @@
+package com.example.springmustachehospital.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    Long id;
+
+    @Column(name = "user_name")
+    String username;
+
+    @Column(name = "password")
+    String password;
+}
---
+++ src/main/java/com/example/springmustachehospital/domain/UserDto.java
@@ -0,0 +1,20 @@
+package com.example.springmustachehospital.domain;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class UserDto {
+    Long id;
+    String username;
+    String password;
+
+    public User toEntity(Long id, String username, String password) {
+        return new User(
+                this.id,
+                this.username,
+                this.password);
+    }
+}
