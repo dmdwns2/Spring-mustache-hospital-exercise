6d57ae3f991b6f9d34a87ee113d84eed212f3b37
dmdwns2 <ssreki1@naver.com>
2022-11-18 14:19:29 +0900

    [스프링부트 핵심가이드] 페이징해서 검색
    1. 주석처리
    2. 폼에 input 넣기
    3. 페이징으로 변경

--- src/main/java/com/example/springmustachehospital/controller/HospitalController.java
+++ src/main/java/com/example/springmustachehospital/controller/HospitalController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,10 +30,21 @@ public class HospitalController {
 //        this.hospitalService = hospitalService;
     }
 
+//    @GetMapping("")
+//    public String list(Model model, Pageable pageable) {
+//        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
+//        log.info("size:{}", hospitals.getSize());
+//        model.addAttribute("hospitals", hospitals);
+//        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+//        model.addAttribute("next", pageable.next().getPageNumber());
+//        return "hospital/list";
+//    }
+
     @GetMapping("")
-    public String list(Model model, Pageable pageable) {
-        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
-        log.info("size:{}", hospitals.getSize());
+    public String list(@RequestParam String keyword, Pageable pageable, Model model) {
+        // keyword는 어떻게 받을 것인가?
+        log.info("keyword:{}", keyword);
+        Page<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining(keyword, pageable);
         model.addAttribute("hospitals", hospitals);
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next", pageable.next().getPageNumber());
--- src/main/java/com/example/springmustachehospital/repository/HospitalRepository.java
+++ src/main/java/com/example/springmustachehospital/repository/HospitalRepository.java
@@ -2,6 +2,7 @@ package com.example.springmustachehospital.repository;
 
 
 import com.example.springmustachehospital.domain.Hospital;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,7 +12,7 @@ public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
 //    List<Hospital> findByTitleContaining(String keyword, Pageable pageable);
 
     List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
-    List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
+    Page<Hospital> findByRoadNameAddressContaining(String keyword, Pageable pageable); // 포함
     List<Hospital> findByRoadNameAddressStartsWith(String keyword); // 시작
     List<Hospital> findByRoadNameAddressEndingWith(String keyword); // 끝남
     List<Hospital> findByPatientRoomCountGreaterThanAndPatientRoomCountLessThan(int var1, int var2);
