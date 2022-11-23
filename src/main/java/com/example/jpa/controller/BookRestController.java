d3badd1f4052f40b4732fac4cdb1f3874341b2d4
dmdwns2 <ssreki1@naver.com>
2022-11-23 14:15:28 +0900

    [스프링부트 핵심가이드] Book, Author 테이블로 책이름과 저자 이름이 나오는 API 만들기
    BookRestController 추가

---
+++ src/main/java/com/example/jpa/controller/BookRestController.java
@@ -0,0 +1,34 @@
+package com.example.jpa.controller;
+
+import com.example.jpa.domain.Book;
+import com.example.jpa.domain.BookResponse;
+import com.example.jpa.service.BookService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookRestController {
+
+    private final BookService bookService;
+
+    public BookRestController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<BookResponse>> list(Pageable pageable){
+        return ResponseEntity.ok().body(bookService.findBooks(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> get(@PathVariable Long id){
+        return ResponseEntity.ok().body(new Book());
+    }
+}
